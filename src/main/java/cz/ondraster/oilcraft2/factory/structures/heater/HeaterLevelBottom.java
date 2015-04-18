9883c2c45d4db6c3deadc1e4813b0d1bb9a6a347
OndraSter <ondra.moravek@hotmail.com>
2015-04-18 17:52:49 +0200

    There we go, even lower level is working

--- src/main/java/cz/ondraster/oilcraft2/factory/structures/heater/HeaterLevelBottom.java
+++ src/main/java/cz/ondraster/oilcraft2/factory/structures/heater/HeaterLevelBottom.java
@@ -1,6 +1,6 @@
 package cz.ondraster.oilcraft2.factory.structures.heater;
 
-import cz.ondraster.oilcraft2.factory.blocks.BlockHeater;
+import cz.ondraster.oilcraft2.factory.FactoryBlocks;
 import cz.ondraster.oilcraft2.multiblock.MultiblockLayer;
 import cz.ondraster.oilcraft2.tools.BlockPos;
 import net.minecraft.world.World;
@@ -10,17 +10,17 @@ public class HeaterLevelBottom extends MultiblockLayer {
     protected boolean checkLevel(World world, BlockPos position) {
         BlockPos baseLevel = position.getBelow();
 
-        if (!BlockHeater.isValid(world, baseLevel.getLeft()))
+        if (!FactoryBlocks.blockHeater.isValid(world, baseLevel.getLeft()))
             return false;
-        if (!BlockHeater.isValid(world, baseLevel.getLeft().getFarther()))
+        if (!FactoryBlocks.blockHeater.isValid(world, baseLevel.getLeft().getFarther()))
             return false;
-        if (!BlockHeater.isValid(world, baseLevel.getFarther()))
+        if (!FactoryBlocks.blockHeater.isValid(world, baseLevel.getFarther()))
             return false;
-        if (!BlockHeater.isValid(world, baseLevel))
+        if (!FactoryBlocks.blockHeater.isValid(world, baseLevel))
             return false;
-        if (!BlockHeater.isValid(world, baseLevel.getRight()))
+        if (!FactoryBlocks.blockHeater.isValid(world, baseLevel.getRight()))
             return false;
-        if (!BlockHeater.isValid(world, baseLevel.getRight().getFarther()))
+        if (!FactoryBlocks.blockHeater.isValid(world, baseLevel.getRight().getFarther()))
             return false;
 
         return true;
